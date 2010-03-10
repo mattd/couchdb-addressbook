b49a248ca04d4e2c1cb3714ada042c0f307e3955
Matt Dawson <matthewtdawson@gmail.com>
2010-03-09 22:51:48 -0500

    Added simple addressbook add form

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import web
 from couchdb.client import Server, PermanentView
 
@@ -16,6 +18,11 @@ class index:
     def GET(self):
         view = PermanentView(db.resource.url + '/_design/results/_view/all', 'all')
         return render.index(view())
+
+    def POST(self):
+        i = web.input()
+        db[uuid4().hex] = {'first_name': i.first_name, 'last_name': i.last_name}
+        raise web.seeother('/')
 
 if __name__ == "__main__":
     app.run()
